9dbcefcebbae8cd0a5282661f89620d9cff3bfa7
Erin Volcan <erinvolcan@gmail.com>
2019-08-14 14:29:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {} 
   
-  pigeon_data.each do |property, property_hash|
+  pigeon_data.each do |property, property_hash| 
    property_hash.each do |values, pigeon_array|
     pigeon_array.each do |name| 
       if pigeon_list[name] 
