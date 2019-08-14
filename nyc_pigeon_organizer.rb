74d259cd6e157f95b96fec90ef42bb9696b901d6
Erin Volcan <erinvolcan@gmail.com>
2019-08-14 14:29:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {} 
   
-  pigeon_data.each do |property, property_hash| 
-   property_hash.each do |values, pigeon_array|
+  pigeon_data.each do |property, property_hash| #access :color and colors-hash
+   property_hash.each do |values, pigeon_array| 
     pigeon_array.each do |name| 
       if pigeon_list[name] 
         if pigeon_list[name][property] 
